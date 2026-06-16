958a46d70711e64ac1e12188c99f2a09decbffeb
Matías Castillo <matcasti@umag.cl>
2026-06-16 13:10:39 -0300

    several changes:
    - HF range increased,
    - safe asymptotic null gain gate,
    - RTS smoother,
    - map priors parameterization,
    - remove geometric projection plot
    - unstandardized innovations for diagnostics

--- R/_test_run.R
+++ R/_test_run.R
@@ -4,23 +4,36 @@
 ## Load function
 source("R/_functions.R")
 
+# -------------------------------------------------------------------------
+
 ## Load test data
-prcp_data <-
-  lapply(
-    X = list.files("data/prcp", pattern = "\\.txt", full.names = TRUE),
-    FUN = function(x) as.numeric(x = readLines(x))
-  )
-
-prcp_batch <- batch_process_hrv(prcp_data, dataset_tag = "PRCP dataset")
-
-report_batch_metrics(prcp_batch)
+rr_data <- (readLines("data/prcp/12726.txt") |> as.numeric())
 
 plot(rr_data, type = "o", pch = "•")
 
-fit <- fit_model(rr_data, jump_power = 0.1)
+fit <- fit_model(rr_data, jump_threshold = 0.1, jump_power = 12)
 
-visualize_model(fit)
+fig <- visualize_model(fit) / diagnose_model(fit) +
+  patchwork::plot_layout(heights = c(1,0.75))
 
-diagnose_model(fit)
+ggsave(filename = "manuscript/_example_fig.png", plot = fig,
+       device = "png", scale = 6, height = 500, width = 500, units = "px")
+
+report_model(fit)
+
+# -------------------------------------------------------------------------
+
+## Load data
+rr_data <- (readLines("data/rri-jabf.txt") |> as.numeric())/1000
+
+plot(rr_data, type = "o", pch = "•")
+
+fit <- fit_model(rr_data, jump_threshold = 0.1, jump_power = 12)
+
+fig <- visualize_model(fit) / diagnose_model(fit) +
+  patchwork::plot_layout(heights = c(1,0.75))
+
+ggsave(filename = "manuscript/_example_fig2.png", plot = fig,
+       device = "png", scale = 6, height = 500, width = 500, units = "px")
 
 report_model(fit)
